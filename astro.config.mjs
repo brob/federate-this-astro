d469ed183e18e36c626dfc1b5af4801bc3adb814
Bryan Robinson <bryanlrobinson@gmail.com>
2023-09-01 13:32:17 -0400

    Tests vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: netlify()
+  adapter: vercel()
 });
