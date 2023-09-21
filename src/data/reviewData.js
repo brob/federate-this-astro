f81a3bfb0a8a6072a23b458bd723c9f183ebeef6
Bryan Robinson <bryanlrobinson@gmail.com>
2023-09-21 12:49:51 -0700

    Adds rudimentary sort by date to reviews

--- src/data/reviewData.js
+++ src/data/reviewData.js
@@ -6,6 +6,7 @@ export const reviews = [
         name: 'Person Personson',
         rating: 4.5,
         comment: 'These shoes are both the mostly comfortable and the most stylish I have ever owned.',
+        date: '2023-02-15'
     },
     {
         id: 4,
@@ -14,6 +15,7 @@ export const reviews = [
         name: 'Second Person',
         rating: 2.5,
         comment: 'These would be better if they were red. I suppose they aren\'t bad, but they aren\'t good either.',
+        date: '2023-04-15'
     },
     {
         id: 5,
@@ -22,6 +24,7 @@ export const reviews = [
         name: 'Another Great Person',
         rating: 4.5,
         comment: 'I\'ve worn these for 3 years and they are still in great shape. I\'m going to buy another pair. I think everyone should have a pair of these shoes in their closet.',
+        date: '2023-04-15'
     },
     {
         id: 2,
@@ -30,6 +33,7 @@ export const reviews = [
         name: 'Green Fan',
         rating: 5,
         comment: 'This is the absolute best hoodie I\'ve ever had',
+        date: '2023-07-15'
     },
     {
         id: 3,
@@ -38,6 +42,7 @@ export const reviews = [
         name: 'Oregan Person',
         rating: 1.5,
         comment: 'After wearing this shirt for 2 days, I realized that it was made of cottom. I am allergic to cotton. I am very disappointed.',
+        date: '2023-07-12'
     },
     {
         id: 6,
@@ -46,6 +51,7 @@ export const reviews = [
         name: 'Another Person',
         rating: 3.5,   
         comment: 'This shirt is okay. It lost a button, but I sewed it back on.',
+        date: '2022-07-15'
     },
     {
         id: 7,
@@ -54,6 +60,7 @@ export const reviews = [
         name: 'Third Person',
         rating: 4.5,
         comment: 'I love this shirt. I wear it all the time.',
+        date: '2023-08-10'
     },
     {
         id: 8,
@@ -62,6 +69,7 @@ export const reviews = [
         name: 'Runner in Rochester',
         rating: 5,
         comment: 'These shoes are great. I\'ve run 100 miles in them and they are still in great shape.',
+        date: '2023-08-10'
     },
     {
         id: 9,
@@ -70,5 +78,6 @@ export const reviews = [
         name: 'Sock Lover',
         rating: 4.5,
         comment: 'These socks are great. They are very colorful and they keep my feet warm.',
+        date: '2023-08-13'
     },
 ]
