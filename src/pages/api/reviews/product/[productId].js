29b1b5da6bec73c910be032549bd10b87c03bad1
Bryan Robinson <bryanlrobinson@gmail.com>
2023-09-21 12:23:28 -0700

    Adds ability to filter by slug to product reviews

--- src/pages/api/reviews/product/[productId].js
+++ src/pages/api/reviews/product/[productId].js
@@ -2,6 +2,9 @@ import {products} from '../../../../data/productData.js'
 
 import {reviews} from '../../../../data/reviewData'
 
+export const prerender = false;
+
+
 
 export function getStaticPaths() {
 	return products.map((item) => {
@@ -9,8 +12,21 @@ export function getStaticPaths() {
 	});
 }
 
+
+function isId(value) {
+    const id = parseInt(value)
+    if (isNaN(id)) {
+        return false
+    }
+    return true
+
+}
+
 export async function GET(request) {
-    const review = reviews.find((item) => item.product === parseInt(request.params.productId))
+    const key = (isId(request.params.productId)) ? 'product' : 'productSlug'
+    const id = (isId(request.params.productId)) ? parseInt(request.params.productId) : request.params.productId
+
+    const review = reviews.filter((item) => item[key] === id)
     return new Response(JSON.stringify({data: review}), {
         headers: {
             'content-type': 'application/json'
