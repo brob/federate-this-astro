2a9d7a23614d7646064657ae9628a13f4c4d0b12
Tim Benniks <tbenniks@gmail.com>
2023-12-04 15:25:48 +0100

    Added SKNCRE graphQL route

---
+++ src/pages/api/graphql/skncre/index.ts
@@ -0,0 +1,54 @@
+export const prerender = false;
+
+import type { APIRoute } from 'astro';
+import { createYoga, createSchema } from 'graphql-yoga';
+import { products } from '../../../../data/skncre/productData'
+
+const schema = createSchema({
+  typeDefs: `
+    type Image {
+      url: String!
+      alt: String
+    }  
+    
+    type Product {
+      id: Int!
+      slug: String!
+      name: String!
+      price: Int
+      description: String
+      shortDescription: String
+      ingredients: String
+      images: [Image]
+      stock: Int
+    }
+
+    type Query {
+      products: [Product!]
+    }    
+  `,
+  resolvers: {
+    Query: {
+      products: () => products,
+    },
+  },
+});
+
+const { handleRequest } = createYoga({
+  schema,
+  graphqlEndpoint: '/api/graphql/skncre',
+  fetchAPI: {
+    Request: Request,
+    Response: Response,
+  },
+});
+
+export const POST: APIRoute = async (context) => {
+  const { request } = context;
+  return handleRequest(request, context);
+};
+
+export const GET: APIRoute = async (context) => {
+  const { request } = context;
+  return handleRequest(request, context);
+};
---
+++ src/pages/routes/skncre.md
@@ -0,0 +1,8 @@
+---
+title: SKNCRE routes
+layout: ../../layouts/page.astro
+---
+
+## SKNCRE products
+
+The SKNCRE cosmetics brand content is served by GraphQL. Please [visit the playground](/api/graphql/skncre) to explore.
