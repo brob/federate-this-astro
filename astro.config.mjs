eea9c647994508f26f1bf58c5688a1bbbea69c22
Bryan Robinson <bryanlrobinson@gmail.com>
2023-12-13 13:05:21 -0500

    Tests just env in the rollup options

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
-        external: ['@components/JsonToSDL.jsx','@components/Env.astro']
+        external: ['@components/Env.astro']
       }
     }
   },
