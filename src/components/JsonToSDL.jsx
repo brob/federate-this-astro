b932d5616e0b8af4d8eee6a2ac6b4c535dc8a588
Bryan Robinson <bryanlrobinson@gmail.com>
2023-12-13 12:27:01 -0500

    Makes page static

--- src/components/JsonToSDL.jsx
+++ src/components/JsonToSDL.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import SubComponent from "@components/SubComponent";
 import { jsonToSchema } from "@walmartlabs/json-to-simple-graphql-schema/lib";
 
 export default function JsonToSDL() {
