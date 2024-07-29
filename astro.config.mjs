5028c5a1abd622cfdecd4e3a0217cbcfd9c676d1
Tim Benniks <tbenniks@gmail.com>
2024-07-29 16:48:50 +0200

    updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,43 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: "hybrid",
   adapter: vercel({
-	functionPerRoute: false,
-
+    functionPerRoute: false,
   }),
-  
-  integrations: [tailwind(),
+
+  integrations: [
+    tailwind(),
     starlight({
-			title: 'Federate This',
-			
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			  ],
-			
-			tableOfContents: {
-				maxHeadingLevel: 4,
-			},
-			social: {
-				github: 'https://github.com/hygraph',
-				twitter: 'https://twitter.com/hygraph',
-			},
-			sidebar: [
-				
-				{
-					label: 'Routes',
-					autogenerate: { directory: 'routes', collapsed: true },
-				},
-				{
-					label: 'Guides',
-					collapsed: true,
-					autogenerate: { directory: 'guides' },
-				}
-			
-	
-			],
-		})]
+      title: "Federate This",
+
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+
+      tableOfContents: {
+        maxHeadingLevel: 4,
+      },
+      social: {
+        github: "https://github.com/hygraph",
+        twitter: "https://twitter.com/hygraph",
+      },
+      sidebar: [
+        {
+          label: "Routes",
+          autogenerate: { directory: "routes", collapsed: true },
+        },
+        {
+          label: "Guides",
+          collapsed: true,
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
 });
